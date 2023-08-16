552c36fb9e6caf1357c200758a71af1502d38aa8
sukma@klob.id <sukma@klob.id>
2023-08-16 23:18:51 +0700

    init about psikotes

---
+++ app/components/Layout/Footer.js
@@ -0,0 +1,69 @@
+"use client";
+
+import Facebook from "../../../public/assets/Icon/facebook.svg";
+import Instagram from "../../../public/assets/Icon/instagram.svg";
+
+export default function Footer() {
+  return (
+    <div className="bg-white-300 pt-44 pb-24">
+      <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
+        <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
+          <p className="mb-4">
+            <strong className="font-medium">Psikotes</strong> adalah platfrom
+            pengembangan diri untuk meningkatkan kesadaran diri, pengetahuan
+            diri, kesejahteraan, dan kebahagiaan hidup.
+          </p>
+          <div className="flex w-full mt-2 mb-8 -mx-2">
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+              <Facebook className="h-6 w-6" />
+            </div>
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md"></div>
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+              <Instagram className="h-6 w-6" />
+            </div>
+          </div>
+          <p className="text-gray-400">
+            ©{new Date().getFullYear()} - Psikotes
+          </p>
+        </div>
+        <div className=" row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-10 flex flex-col">
+          <p className="text-black-600 mb-4 font-medium text-lg">Product</p>
+          <ul className="text-black-500 ">
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Tes Preferensi Energi.{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Tes Preferensi Informasi.{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Tes Preferensi Keputusan.{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Tes Preferensi Lingkungan.{" "}
+            </li>
+          </ul>
+        </div>
+        <div className="row-span-2 sm:col-span-2 sm:col-start-10 sm:col-end-13 flex flex-col">
+          <p className="text-black-600 mb-4 font-medium text-lg">Engage</p>
+          <ul className="text-black-500">
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              FAQ (soon){" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Tutorials (soon){" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              About Us{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Privacy Policy (soon){" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Terms of Service (soon){" "}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/tes-kepribadian/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>TEST KEPRIBADIAN</h1>
+    </main>
+  );
+}
