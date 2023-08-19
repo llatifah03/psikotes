8f859e4da1a130ed9771ba6ea6a5b5a3e3762dfc
sukma@klob.id <sukma@klob.id>
2023-08-19 15:25:15 +0700

    test env local

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,34 +1,28 @@
-// import RemoveBtn from "@/components/Button/RemoveButton";
-// import toast from "react-hot-toast";
-// import getConfig from "next/config";
+import RemoveBtn from "@/components/Button/RemoveButton";
+import toast from "react-hot-toast";
 
-// const { publicRuntimeConfig } = getConfig();
-// const baseUrl = `${publicRuntimeConfig.apiUrl}/users`;
+const getUsers = async () => {
+  try {
+    const res = await fetch(`${process.env.PATH_URL}api/user`, {
+      cache: "no-store",
+    });
 
-// const getUsers = async () => {
-//   try {
-//     const res = await fetch(`http://localhost:3000/api/user`, {
-//       cache: "no-store",
-//     });
-
-//     if (!res.ok) {
-//       toast.error("Failed to get user");
-//     }
-//     return res.json();
-//   } catch (error) {
-//     console.log("getUsers error", error.message);
-//     toast.error(error);
-//   }
-// };
+    if (!res.ok) {
+      toast.error("Failed to get user");
+    }
+    return res.json();
+  } catch (error) {
+    console.log("getUsers error", error.message);
+    toast.error(error);
+  }
+};
 
 export default async function Dashboard() {
-  // const { users } = await getUsers();
+  const { users } = await getUsers();
 
   return (
     <div className="md:flex flex-col max-w-screen-xl mt-24 px-8 xl:px-16 mx-auto justify-center">
-      Dashboard
-      {/* {console.log("props", props)} */}
-      {/* {users?.map((user) => (
+      {users?.map((user) => (
         <div
           key={user._id}
           className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
@@ -43,7 +37,7 @@ export default async function Dashboard() {
             <RemoveBtn id={user._id} />
           </div>
         </div>
-      ))} */}
+      ))}
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Feature from "@/components/Feature";
+import ListPsikotes from "@/components/ListPsikotes";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Feature />
+      {/* <Feature /> */}
+      <ListPsikotes />
     </>
   );
 }
