1c9c09285d7ce7a123dc6b0676631a9f496019cc
sukma@klob.id <sukma@klob.id>
2023-09-11 08:27:40 +0700

    test deploy error dashboard

--- app/middleware.jsx
+++ app/middleware.jsx
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 // fungsi untuk page yg butuh auth,
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/protected/:path*"],
 };
